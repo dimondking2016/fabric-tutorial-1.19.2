50399753e7e4780db562848bdaa90993ee2d3c7d
dimondking2016 <dimondking2018@gmail.com>
2023-02-01 15:42:21 +1300

    added a very important comment

--- src/main/java/net/dk16/tutorialmod/TutorialMod.java
+++ src/main/java/net/dk16/tutorialmod/TutorialMod.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
+	//Very important comment
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger("tutorialmod");
 
