4ac6ea3aa0bc9a85b40d49922fe735cb24e5e06d
dimondking2016 <dimondking2018@gmail.com>
2023-02-01 16:24:28 +1300

    starting mod items

---
+++ src/main/java/net/dk16/tutorialmod/item/ModItems.java
@@ -0,0 +1,14 @@
+package net.dk16.tutorialmod.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.registry.Registries;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    private static Item registerItem(String name, Item item) {
+        return Registry(Registries.ITEM)
+    }
+
+
+}
