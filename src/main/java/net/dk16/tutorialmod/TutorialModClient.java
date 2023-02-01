9a5da925227f6a19826ac874ebe8f4d32a1bcace
dimondking2016 <dimondking2018@gmail.com>
2023-02-01 15:35:52 +1300

    starting project

---
+++ src/main/java/net/dk16/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.dk16.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @java.lang.Override
+    public void onInitializeClient() {
+
+    }
+}
